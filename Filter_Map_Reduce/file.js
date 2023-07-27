77dd106aa16f3068ef6af65a5a390efe69bb0ec6
Deepak <deepaknnayak34@gmail.com>
2023-07-27 23:17:47 +0530

    code with examples added

--- Filter_Map_Reduce/file.js
+++ Filter_Map_Reduce/file.js
@@ -24,3 +24,73 @@ Biodata.filter((e)=>{
         //console.log(e.name)
     }
 })
+
+
+const nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const result = nums.filter((e)=>{
+    //return e % 3 == 0;
+})
+
+// console.log(result)
+
+const num_by_four = nums.filter((num) => (num % 4 ==0))
+//console.log(num_by_four)
+
+const rslt = nums.map((e)=>{
+    if(e % 3 ==0){
+        //console.log(e)
+    }
+})
+
+const abc = nums.forEach((e)=>{
+    if(e % 5 === 0){
+        // console.log(e)
+    }
+})
+
+// Map 
+const addNumber = [1,2,3,4,5,6,7,8]
+
+const add = addNumber.map((num)=>{
+    return num+10
+})
+console.log(add)
+
+// Map Chaining
+
+const addnum = addNumber.map((num)=>{
+    return num+10
+}).map((num)=>{
+    return num+2
+})
+//console.log(addnum)
+
+// Using Filter In Chaining
+const numdivby2 = addNumber.map((num)=>{
+    return num+10
+}).map((num)=>{
+    return num+2
+}).filter((num)=>{
+    return num % 2 == 0
+})
+
+//console.log(numdivby2)
+
+// Reduce
+const reduce = addNumber.reduce((acc, curr)=>{
+    return acc+curr
+},0)
+
+console.log(reduce)
+
+
+
+
+
+
+
+
+
+
+
+
