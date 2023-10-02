b848681ec68d49f96752d71bdbe18f1a99a94aa3
Deepak <deepaknnayak34@gmail.com>
2023-10-02 11:26:43 +0530

    updated

--- Closure/index.js
+++ Closure/index.js
@@ -2,13 +2,26 @@
 //---------
 
 // a function can return another function
+// closure is a combination of function along with lexical environment within which that funcion is declared. 
+// in other words closure give you access to the outer scope from its inner scope
+// it can access the variables as well as the parameter from the outer scope.
+// bundeled function mean function ke andar function jo apka inner function hota hota hai 
+// uske pass power hota hei ki woh outer function ki variable or parameter ko access kar sakhta hai.
 
-function printFullName(firstname,lastname){
-    function fullName(){
-        console.log(`${firstname} ${lastname}`)
-    }
-    return fullName;
+// Lexical Scope is the ability of a function to access the variable from its inner scope.
+
+// function printFullName(firstname,lastname){
+//     function fullName(){
+//         console.log(`${firstname} ${lastname}`)
+//     }
+//     return fullName;
+// }
+
+// const result = printFullName('deepak kumar','nayak')
+// console.log(result)
+
+function printFullName(){
+    console.log('hello')
 }
 
-const result = printFullName('deepak kumar','nayak')
-console.log(result)
+printFullName()
