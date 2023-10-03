18e1bca448a3ce1d55c67a3bd76c70f454e15290
Deepak <deepaknnayak34@gmail.com>
2023-10-03 13:48:06 +0530

    updated

---
+++ let_const_var/index.js
@@ -0,0 +1,9 @@
+function isLogin(){
+    if(isLogin){
+        var name  = "deepak"
+        var position = "FullStack Developer"
+    }
+    console.log(`${name} working as a ${position}` )
+}
+
+isLogin(true)
