bb885bcc46bd0cfb8768a7099e621d54aeb46a32
Deepak <deepaknnayak34@gmail.com>
2023-10-03 22:09:52 +0530

    updated

--- Hoisting/index.js
+++ Hoisting/index.js
@@ -1,8 +1,9 @@
-getName()
+//getName()
 console.log(x)
-console.log(getName)
+//console.log(getName)
 
 var x = 10
+const b = 20
 var getName = () =>{
     console.log('Chai aur Code With Javascipt')
 }
