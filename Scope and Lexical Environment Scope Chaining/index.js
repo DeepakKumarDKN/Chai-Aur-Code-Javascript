c122062c39b0391fb5068edf26ddfebb573b7b84
Deepak <deepaknnayak34@gmail.com>
2023-10-05 16:43:16 +0530

    updated

---
+++ Scope and Lexical Environment Scope Chaining/index.js
@@ -0,0 +1,28 @@
+// function name(){
+//     console.log(username)
+// }
+
+// let username = "deepak"
+// name()
+
+// function name(){
+//     function getuserName(){
+//         console.log(username)
+//     }
+//     getuserName()
+// }
+
+// let username = "deepak"
+// name()
+
+function name(){
+    let username = "deepak"
+    function getuserName(){
+        console.log(username)
+    }
+    getuserName()
+}
+
+name()
+
+
